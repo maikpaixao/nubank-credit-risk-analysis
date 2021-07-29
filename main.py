3e509941f37a59eba7f239ba23483fdb6c0bc9da
Maik Luiz Paixão <paixomaik@outlook.com.br>
2021-07-29 11:42:00 -0300

    code refactored

--- main.py
+++ main.py
@@ -2,38 +2,15 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.ensemble import RandomForestClassifier
-from sklearn import preprocessing
-from utils import Utils
+from preprocess import Preprocess
+from model import Model
 
-le = preprocessing.LabelEncoder()
+if __name__ == '__main__':
+  data = pd.read_csv('data/training_data.csv')
+  preprocess = Preprocess(data)
+  cleaned_data = preprocess.clean_data()
 
-def transform():
-  data['score_1'] = le.fit_transform(data['score_1'])
-  data['score_1'] = data['score_1'].astype('float64')
-
-  data['score_2'] = le.fit_transform(data['score_2'])
-  data['score_2'] = data['score_2'].astype('float64')
-
-  data['job_name'] = le.fit_transform(data['job_name'])
-  data['job_name'] = data['job_name'].astype('float64')
-
-  data['target_default'] = le.fit_transform(data['target_default'])
-  data['target_default'] = data['target_default'].astype('float64')
-
-data = pd.read_csv('data/training_data.csv')
-data = data[['target_default', 'score_1', 'score_2', 'score_3', 'risk_rate', 'last_amount_borrowed',
-            'last_borrowed_in_months', 'credit_limit', 'income',  'job_name', 'n_bankruptcies', 
-            'n_accounts', 'n_issues', 'external_data_provider_credit_checks_last_2_year']]
-
-#preprocessing
-transform()
-data = data.fillna(data.mean())
-#corr = data.corr()
-
-utils = Utils(data)
-#acc = utils.train()
-
-print(utils.train())
-#sns.heatmap(corr)
-#plt.show()
+  model = Model(cleaned_data)
+  score = model.cross_validation()
+  
+  print(np.mean(score))
--- model.py
+++ model.py
@@ -8,26 +8,23 @@ class Model:
     self.x = encoded_df.drop('target_default', axis=1).select_dtypes(exclude='object')
     self.y = encoded_df['target_default']
   
+  def train(self, x_train, y_train):
+    model = XGBClassifier(learning_rate=0.0145, n_estimators=200, max_depth=6,
+                            subsample=1.0, colsample_bytree=1.0, gamma=1, random_state=0, n_jobs=1)
+    model.fit(x_train, y_train)
+    return model
+  
   def cross_validation(self, k=3):
-    # Verificando nossa métrica com XGBClassifier
-    # Com 5 repetições para cada divisão dos dados em 3 splits
     resultados = []
     for rep in range(5):
-      print ('Repetição :',rep)
       kf = KFold(n_splits=k, shuffle=True, random_state = rep)
       
       for linhas_treino, linhas_teste in kf.split(self.x):
-        print('Treino :', linhas_treino.shape[0])
-        print('Teste :', linhas_teste.shape[0])
-
-        X_train, X_test = self.x.iloc[linhas_treino], self.x.iloc[linhas_teste]
+        x_train, x_test = self.x.iloc[linhas_treino], self.x.iloc[linhas_teste]
         y_train, y_test = self.y.iloc[linhas_treino], self.y.iloc[linhas_teste]
 
-        ml_model = XGBClassifier(learning_rate=0.0145, n_estimators=1000, max_depth=6, subsample=1.0, colsample_bytree=1.0, gamma=1, 
-                            random_state=0, n_jobs=1) # 84.84
-        ml_model.fit(X_train, y_train)
-        p = ml_model.predict(X_test)
-
-        acc = np.mean(y_test == p)
+        model = self.train(x_train, y_train)
+        pred = model.predict(x_test)
+        acc = np.mean(y_test == pred)
         resultados.append(acc)
     return resultados
